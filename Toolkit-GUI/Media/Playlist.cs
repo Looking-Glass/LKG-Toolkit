9eb28ba4a9f2e6456c670ed9da5cd0f73aa50f00
Alec Zinsli <zinsli.alec@gmail.com>
2024-02-16 14:22:32 -0500

    missed one thing in toolkit gui

--- Toolkit-GUI/Media/Playlist.cs
+++ Toolkit-GUI/Media/Playlist.cs
@@ -134,7 +134,7 @@ namespace ToolkitGUI.Media
                 {
                     bridgePlaylist.AddRGBDItem(Path.GetFullPath(item.path), item.rows, item.cols, item.aspect, 
                         item.depthiness, item.depth_cutoff, item.focus, item.depth_loc, 0, 0, 
-                        item.zoom, new System.Numerics.Vector2(item.crop_pos_x, item.crop_pos_y), 
+                        item.zoom, new System.Numerics.Vector2(item.crop_pos_x, item.crop_pos_y), new System.Numerics.Vector2(), 
                         item.depth_inversion == 1, item.chroma_depth == 1, item.durationMS);
                 }
                 else
